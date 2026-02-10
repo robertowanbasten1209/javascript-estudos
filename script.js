06f21ad20b071d9e51fc9ab654d8ea41efcdbc05
Roberto Wanbasten <you@example.com>
2026-02-10 19:04:06 -0300

    Dia 10 - if/else e verificador de idade

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-let nome = "Wanbasten"
-alert("Bem-vindo " + nome + " !")
-console.log("Usuário digitou:", nome)
+function verificar() {
+  let idade = Number(prompt("Digite sua idade:"))
+
+  let resultado = document.querySelector("#resultado")
+
+  if (idade >= 18) {
+    resultado.innerText = "Voce é maior de idade"
+  } else {
+    resultado.innerText = "Voce é menor de idade"
+  }
+}
